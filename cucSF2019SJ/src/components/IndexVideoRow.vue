2473c9995cc2fe31b6d3e5d989364731d383ca0f
Ash-one <1047141957@qq.com>
2019-07-12 00:29:54 +0800

    add json and static pics

--- cucSF2019SJ/src/components/IndexVideoRow.vue
+++ cucSF2019SJ/src/components/IndexVideoRow.vue
@@ -1,18 +1,24 @@
 <template>
+  
   <el-row>
+    <!-- 取消防盗链，拒绝检测访问图片的referrer -->
+    <meta name="referrer" content="no-referrer" />
     <el-col :span="11" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0">
       <el-card :body-style="{ padding: '0px' }">
-        <!-- 封面URL403 -->
-        <img v-bind:src="videoMsgs[index].url" class="image">
+        <!-- 封面 -->
+        <img v-bind:src="videoMsgs[index].pic" class="image">
         <div style="padding: 14px;">
-          <span>{{videoMsgs[index].name}}</span>
+          <!-- 标题 -->
+          <span>{{videoMsgs[index].title}}</span>
           <div class="bottom clearfix">
-            <span class="subArea">{{videoMsgs[index].subArea}}</span>
+            <!-- 作者（没有分区） -->
+            <span class="subArea">{{videoMsgs[index].author}}</span>
           </div>
         </div>
       </el-card>
     </el-col>
   </el-row>
+
 </template>
 
 
